c5d476bacd7cb2b46da2aa74ae519ae234126511
Tom Hicks <hickst@tohono.com>
2018-11-28 16:32:02 -0700

    Redo uploader shell script to require only the data directory.

--- uploadToIRods.sh
+++ uploadToIRods.sh
@@ -2,62 +2,28 @@
 
 # Bash script to take parameters and launch the Astrolabe Uploader program.
 #   Written by: Tom Hicks. 11/12/2018.
+#   Last Modified: Redo to require only the data directory.
 #
 progname=`basename $0`
 
-IRODS_ENV_JSON=irods_environment.json
-IRODS_PASSWORD=.irodsA
-
 Usage() {
-  echo "Usage: $progname irods-directory data-directory"
+  echo "Usage: $progname data-directory"
   echo "where"
-  echo "    irods-directory: a directory containing iRods configuration files (usually: ~/.irods)"
-  echo "     data-directory: a directory containing FITS files to be uploaded to the datastore"
-  exit 1
+  echo "  data-directory is a directory containing FITS files to be uploaded to the iRods datastore"
 }
 
-hasIrodsConfigFiles () {
-  if [ ! -d "$1" ]; then
-    return 1
-  elif [ ! -r "${1}/${IRODS_ENV_JSON}" ]; then
-    return 2
-  elif [ ! -r "${1}/${IRODS_PASSWORD}" ]; then
-    return 3
-  else
-    return 0
-  fi
-}
-
-if [ $# -lt 2 ]; then
-  echo "Error: Please specify the iRods configuration file directory and data file directory"
+if [ $# -lt 1 ]; then
+  echo "Error: Please specify a data file directory containing FITS files to be uploaded"
   Usage
+  exit 1
 fi
 
-DATA_DIR=${2}
-if [ ! -d "$DATA_DIR" ]; then
+DATA_DIR=${1}
+if [ ! -r "$DATA_DIR" ]; then
   echo "Error: Unable to find or read the specified data directory '$DATA_DIR'"
   Usage
+  exit 2
 fi
 
-IRODS_DIR=${1}
-hasIrodsConfigFiles "$IRODS_DIR"
-case "$?" in
-  0) ;;
-  1)
-    echo "Error: Unable to find or read the specified iRods configuration directory '$IRODS_DIR'"
-    Usage
-    ;;
-  2)
-    echo "Error: The file '${IRODS_ENV_JSON}' is missing from the iRods configuration directory '$IRODS_DIR'"
-    exit 2
-    ;;
-  3)
-    echo "Error: The file '${IRODS_PASSWORD}' is missing from the iRods configuration directory '$IRODS_DIR'"
-    exit 3
-    ;;
-esac
-
-# echo "IRODS: ${IRODS_DIR}"
-# echo " DATA: ${DATA_DIR}"
-
-docker run -it --rm --name uploader -v${IRODS_DIR}:/home/jovyan/.irods -v${DATA_DIR}:/home/jovyan/data uploader-de
+echo "Uploading FITS files from '${DATA_DIR}'..."
+docker run -it --rm --name uploader -v${DATA_DIR}:/home/jovyan/data hickst/uploader-de
