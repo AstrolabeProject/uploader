6861cbddb12b2e3e76fbc72b5b5f672afb90ea9a
Tom Hicks <hickst@tohono.com>
2019-02-07 19:02:54 -0700

    Update for rebase on Ubuntu. Better data directory checking. Make data directory path absolute.

--- uploadToIRods.sh
+++ uploadToIRods.sh
@@ -2,14 +2,15 @@
 
 # Bash script to take parameters and launch the Astrolabe Uploader program.
 #   Written by: Tom Hicks. 11/12/2018.
-#   Last Modified: Update for project rename.
+#   Last Modified: Update for rebase on Ubuntu. Better data dir checking. Expand data dir path.
 #
 progname=`basename $0`
 
 Usage() {
-  echo "Usage: $progname data-directory"
+  echo "Usage: $progname data-directory [irods-username]"
   echo "where"
   echo "  data-directory is a directory containing FITS files to be uploaded to the iRods datastore"
+  echo "  irods-username is your Cyverse username (defaults to your current username on this host)"
 }
 
 if [ $# -lt 1 ]; then
@@ -19,11 +20,19 @@ if [ $# -lt 1 ]; then
 fi
 
 DATA_DIR=${1}
-if [ ! -r "$DATA_DIR" ]; then
-  echo "Error: Unable to find or read the specified data directory '$DATA_DIR'"
+if [ ! -d "$DATA_DIR" ]; then
+  echo "Error: the specified data directory '$DATA_DIR' does not exist or is not a directory"
   Usage
   exit 2
 fi
+if [ ! -r "$DATA_DIR" ]; then
+  echo "Error: the specified data directory '$DATA_DIR' is not readable"
+  Usage
+  exit 3
+fi
+DATA_PATH=$(cd ${DATA_DIR} && pwd)
 
-echo "Uploading FITS files from '${DATA_DIR}'..."
-docker run -it --rm --name uploader -v${DATA_DIR}:/home/jovyan/data hickst/uploader
+IUSER=${2:-$USER}
+
+echo "Uploading FITS files from '${DATA_PATH}' for user '${IUSER}'..."
+docker run -it --rm --name uploader -e IPLANT_USER=${IUSER} -v ${DATA_PATH}:/data hickst/uploader
